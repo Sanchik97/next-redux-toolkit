474caa6daa113ef6976f195f804193e919cf4cac
Sanjar Fazyiev <sounzh@MacBook-Pro-Sanjar.local>
2020-12-24 17:23:09 +0500

    select margin right

--- components/todo-filter/TodoFilter.tsx
+++ components/todo-filter/TodoFilter.tsx
@@ -14,7 +14,6 @@ const TodoFilter: React.FC<props> = () => {
 	const dispatch = useDispatch()
 	const {t} = useTranslation()
 	return (
-
 		<Form.Item label={t('todo:filter')}>
 			<Input
 				placeholder={t('todo:filter')}
